5e2c4a4ce7daf28631eeb17dda4f9301cf446476
Nasib Farooq <nasibfarooq@outlook.com>
2024-07-18 12:55:36 +0530

    calculator

---
+++ Calculator/script.js
@@ -0,0 +1,37 @@
+let operands = document.querySelectorAll(".operand");
+let operators = document.querySelectorAll(".operator");
+let display = document.querySelector("#display");
+let equalToBTN = document.querySelector("#equalTo");
+let clearBTN = document.querySelector("#clear");
+let backspaceBTN = document.querySelector("#backspace");
+
+operands.forEach((el) => {
+  el.addEventListener("click", () => {
+    display.value += el.value;
+  });
+});
+
+operators.forEach((el) => {
+  el.addEventListener("click", () => {
+    display.value += el.value;
+  });
+});
+
+const evalvate = () => {
+  if (display.value !== "") {
+    try {
+      let result = eval(display.value);
+      display.value = result;
+    } catch (e) {
+      display.value = "Error";
+    }
+  }
+};
+
+equalToBTN.addEventListener("click", evalvate);
+clearBTN.addEventListener("click", () => {
+  display.value = "";
+});
+backspaceBTN.addEventListener("click", () => {
+  display.value = display.value.slice(0, -1);
+});
